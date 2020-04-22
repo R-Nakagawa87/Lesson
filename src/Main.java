83504bd1415ccca210acdb0ff6682188a05c82b3
edu11 <edu11@EDUPC11.n00d01.kis.co.jp>
2020-04-22 14:19:19 +0900

    平均点を求めました

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		String name = "Nakagawa";
+		//テスト
+		String name = "Ryosuke";
 		System.out.println("名前は" + name + "です");
 
 		//日付を表示
@@ -12,15 +12,55 @@ public class Main {
 
 		double pi = 3.14;
 		System.out.println("円周率は" + pi + "です");
+
+		//計算の文
+		int a = 20;
+		int b = a + 5;
+		System.out.println(a);
+		System.out.println(b);
+
+		//\"エスケープシーケンス\"
+		System.out.println("今日は\n\"4月\"です");
+		System.out.println("\\100");
+		System.out.println("C:\\に\"フォルダ\"を作成");
+
+		//演算子
+		int num = 10;
+		int amari = num%3;	//10%3
+		System.out.println(num + "%3の結果は" + amari);
+		num += 5;	//num = 15
+		amari = num%3;	//15%3
+		System.out.println(num + "%3の結果は" + amari);
+		num++;	//インクリメント
+		System.out.println("num++の結果は" + num);
+
+		//型の変換
+		double d = 3;
+		int n = (int)3.0;
+		String s = "ベスト" + 3;
+
+		int kokugo = 70, sansu = 75;
+		double heikin = ((double)kokugo + (double)sansu) / 2;
+		System.out.println("国語(70点)と算数(75点)の平均は" + heikin + "点");
+
 	}
 
+	static public void lesson4_22() {
+
+	}
+
+	static public void lesson4_23() {
+
+	}
+
+	static public void lesson4_24() {
+
+	}
 
 	static public void ensyu1_2(){
 	    //練習1-2
 	    int a = 3, b = 5;
 	    int c = a*b;
-	    System.out.println("");
-	    System.out.println("練習1-2");
 	    System.out.println(c);
 	}
 
