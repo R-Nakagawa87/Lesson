d5900f9ab674136c6bbc715a17f937f833e1b726
edu11 <edu11@EDUPC11.n00d01.kis.co.jp>
2020-04-22 16:57:11 +0900

    細かいところを修正

--- src/Kuku.java
+++ src/Kuku.java
@@ -5,18 +5,19 @@ public class Kuku {
 		System.out.println("九九の練習をします");
 		System.out.println("答えを入力してください");
 
+		//計算に使う2つの数を発生
 		int r1 = new java.util.Random().nextInt(9) + 1;
 		int r2 = new java.util.Random().nextInt(9) + 1;
 
 		System.out.println(r1 + "*" + r2 + "=");
 
-		int trueKotae = r1*r2;
-		int scanKotae = new java.util.Scanner(System.in).nextInt();
+		int trueKotae = r1*r2;	//正しい答え
+		int scanKotae = new java.util.Scanner(System.in).nextInt();	//入力された答え
 
 		if (scanKotae == trueKotae ) {
-			System.out.println(scanKotae + " 正解");
+			System.out.println(" 正解");
 		}else {
-			System.out.println(scanKotae + " 不正解");
+			System.out.println(" 不正解");
 			System.out.println("正解は" + trueKotae);
 		}
 	}
