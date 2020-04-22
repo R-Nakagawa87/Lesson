8a996d6195707fcd02c01624eb9a84d46c756e5d
edu11 <edu11@EDUPC11.n00d01.kis.co.jp>
2020-04-22 13:19:41 +0900

    1st

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "Nakagawa";
+		System.out.println("名前は" + name + "です");
+		
+		//日付を表示
+		int tuki = 4, hi = 22;
+		System.out.println("今日は" + tuki + "月" + hi + "日です");
+		
+		double pi = 3.14;
+		System.out.println("円周率は" + pi + "です");
+	}
+
+	
+	static public void ensyu1_2(){
+	    //練習1-2
+	    int a = 3, b = 5;
+	    int c = a*b;
+	    System.out.println("");
+	    System.out.println("練習1-2");
+	    System.out.println(c);
+	}
+	
+	static public void ensyu1_3(){
+	    //練習1-3
+	    boolean b = true;
+	    char c = '駆';
+	    double d = 3.14;
+	    long l = 314159265853979L;
+	    String s = "ミナトに攻撃！敵に15ポイントの攻撃を与えた。";
+	    System.out.println(b);
+	    System.out.println(c);
+	    System.out.println(d);
+	    System.out.println(l);
+	}    
+	
+}
